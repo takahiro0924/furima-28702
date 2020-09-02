9601683edf63c0270083b3b44d3fc378fed7602e
takahiro0924 <n.takahiro0924@gmail.com>
2020-09-02 12:41:06 +0900

    Rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name,:family_name, :first_name, :family_furigana, :first_furigana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :family_name, :first_name, :family_furigana, :first_furigana, :birthday])
   end
 end
