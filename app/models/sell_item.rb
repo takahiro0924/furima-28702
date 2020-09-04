f712e223475f482d84abba5a6bb2a869830a6f63
takahiro0924 <n.takahiro0924@gmail.com>
2020-09-04 17:00:30 +0900

    Acivehashの実装

--- app/models/sell_item.rb
+++ app/models/sell_item.rb
@@ -1,3 +1,14 @@
 class SellItem < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :area
+  belongs_to_active_hash :category
+  belongs_to_active_hash :day
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :status
+
+  validates :name, :explanation, :area_id, :category_id, :day_id, :delivery_fee_id, :status_id, :price, :user_id, presence: true
+
+  validates :area_id, :category_id, :day_id, :delivery_fee_id, :status_id, numericality: { other_than: 0 } 
 end
