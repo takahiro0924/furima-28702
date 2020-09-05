45d6bfae681cd33887a41b752e8046df2380470e
takahiro0924 <n.takahiro0924@gmail.com>
2020-09-06 02:20:43 +0900

    価格範囲、半角英数指定

--- app/models/sell_item.rb
+++ app/models/sell_item.rb
@@ -12,4 +12,8 @@ class SellItem < ApplicationRecord
   validates :name, :image, :explanation, :area_id, :category_id, :day_id, :delivery_fee_id, :status_id, :price, :user_id, presence: true
 
   validates :area_id, :category_id, :day_id, :delivery_fee_id, :status_id, numericality: { other_than: 0 } 
+
+  with_options presence: true, format: { with: /3[0-9][0-9]|9[0-9][0-9][0-9][0-9][0-9][0-9]/, message: '価格の範囲を半角英数、¥300~¥9,999,999の間で指定てください' } do
+    validates :price
+  end
 end
