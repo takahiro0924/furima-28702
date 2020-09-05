39e34226e05f3e9a4716a6146bc52597b4e1f85d
takahiro0924 <n.takahiro0924@gmail.com>
2020-09-06 01:13:16 +0900

    ActiveHash,ActiveStorage

--- app/models/sell_item.rb
+++ app/models/sell_item.rb
@@ -7,6 +7,7 @@ class SellItem < ApplicationRecord
   belongs_to_active_hash :day
   belongs_to_active_hash :delivery_fee
   belongs_to_active_hash :status
+  has_one_attached :image
 
   validates :name, :explanation, :area_id, :category_id, :day_id, :delivery_fee_id, :status_id, :price, :user_id, presence: true
 
