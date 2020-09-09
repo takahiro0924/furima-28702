dddc5a693b550c3d105dcc290b9159a840e65b64
takahiro0924 <n.takahiro0924@gmail.com>
2020-09-09 20:10:54 +0900

    商品一覧表示

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,30 @@
+class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
+  def index
+    @sell_item = SellItem.includes(:user)
+  end
+
+  def new
+    @sell_item = SellItem.new
+  end
+
+  def create
+    @sell_item = SellItem.new(sell_item_params)
+    if @sell_item.save
+      redirect_to action: :index
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def sell_item_params
+    params.require(:sell_item).permit(:name, :explanation, :category_id, :status_id, :delivery_fee_id, :area_id, :day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+end
