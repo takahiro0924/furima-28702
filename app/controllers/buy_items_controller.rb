29c1a49af42094dfd3bdcaccc60a4fb5627a3936
takahiro0924 <n.takahiro0924@gmail.com>
2020-09-16 20:10:20 +0900

    before_actioinを定義

--- app/controllers/buy_items_controller.rb
+++ app/controllers/buy_items_controller.rb
@@ -1,11 +1,11 @@
 class BuyItemsController < ApplicationController
+  before_action :set_sell_item, only: [:index, :create]
+
   def index
-    @sell_item = SellItem.find(params[:item_id])
     @buy_item = BuyItemsInfo.new
   end
 
   def create
-    @sell_item = SellItem.find(params[:item_id])
     @buy_item = BuyItemsInfo.new(buy_item_params)
     if @buy_item.valid?
       pay_item
@@ -15,6 +15,8 @@ class BuyItemsController < ApplicationController
       render 'index'
     end
   end
+
+  private
 
   def buy_item_params
     params.require(:buy_items_info).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, sell_item_id: params[:item_id], token: params[:token])
@@ -29,4 +31,9 @@ class BuyItemsController < ApplicationController
       currency: 'jpy'                 # 通貨の種類(日本円)
     )
   end
+
+  def set_sell_item
+    @sell_item = SellItem.find(params[:id])
+  end
+
 end
