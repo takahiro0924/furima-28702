61460ab2d6070c7ffa543927e3b206dd2e3f7bf8
takahiro0924 <n.takahiro0924@gmail.com>
2020-09-16 11:35:54 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "items#index"
   
   resources :items do
-    resources :buy_items, only: :index
+    resources :buy_items, only:  [:index, :create]
   end
 end
