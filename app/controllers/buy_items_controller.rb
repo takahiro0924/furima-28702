2d66f4f7008037449fe2ce894991258283eacfbe
takahiro0924 <n.takahiro0924@gmail.com>
2020-09-16 16:29:24 +0900

    環境変数の設定

--- app/controllers/buy_items_controller.rb
+++ app/controllers/buy_items_controller.rb
@@ -17,12 +17,12 @@ class BuyItemsController < ApplicationController
   end
 
   def buy_item_params
-    params.require(:buy_items_info).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number,).merge(user_id: current_user.id, sell_item_id: params[:item_id], token: params[:token])
+    params.require(:buy_items_info).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, sell_item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
     @sell_item = SellItem.find(params[:item_id])
-    Payjp.api_key = "sk_test_b2abf0acecb48056e711a74e"  # PAY.JPテスト秘密鍵
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
       amount: @sell_item.price,  # 商品の値段
       card: buy_item_params[:token],    # カードトークン
