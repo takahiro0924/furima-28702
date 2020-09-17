20039dff77110ff52b81a2bd351317d282e579f0
takahiro0924 <n.takahiro0924@gmail.com>
2020-09-17 10:32:31 +0900

    basic認証とデプロイgem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters,  if: :devise_controller?
+  before_action :basic_auth
+   private
 
-  protected
-
-  def configure_permitted_parameters
+   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :family_name, :first_name, :family_furigana, :first_furigana, :birthday])
-  end
+   end
+
+   def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+   end
 end
