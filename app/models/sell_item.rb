03d9a00da093def1b50da26cbee942d2482ba53d
takahiro0924 <n.takahiro0924@gmail.com>
2020-09-25 12:26:50 +0900

    日本語表記変更

--- app/models/sell_item.rb
+++ app/models/sell_item.rb
@@ -1,6 +1,7 @@
 class SellItem < ApplicationRecord
   belongs_to :user
   has_one :buy_item
+  has_many :comment
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :area
@@ -12,7 +13,8 @@ class SellItem < ApplicationRecord
 
   validates :name, :image, :explanation, :area_id, :category_id, :day_id, :delivery_fee_id, :status_id, :price, presence: true
 
-  validates :area_id, :category_id, :day_id, :delivery_fee_id, :status_id, numericality: { other_than: 0 }
+  validates :area_id, :category_id, :day_id, :delivery_fee_id, :status_id, numericality: { other_than: 0, message: "を選択してください" }
 
   validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 9_999_999 }
+  
 end
